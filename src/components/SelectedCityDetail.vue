dfb5e813985d11507d6271e962556995fb604863
Alex Dodd <alexdodd@gmail.com>
2021-01-16 16:46:38 +0000

    minor formatting

--- src/components/SelectedCityDetail.vue
+++ src/components/SelectedCityDetail.vue
@@ -1,11 +1,14 @@
 <template>
-  <div>
-    <h1>Name: {{ selectedCity.name }}, {{ selectedCity.sys.country }}</h1>
+  <div v-if="selectedCity.cod != '404'">
+    <h1>City: {{ selectedCity.name }}, {{ selectedCity.sys.country }}</h1>
     <h3>
       lat: {{ selectedCity.coord.lat }} long: {{ selectedCity.coord.lon }}
     </h3>
     <h3>Current Weather: {{ selectedCity.weather[0].description }}</h3>
-    <h3>Current Temp: {{ roundToDp(selectedCity.main.temp - 273.15, 0) }}</h3>
+    <h3>Current Temp: {{ roundToDp(selectedCity.main.temp - 273.15, 0) }}°</h3>
+  </div>
+  <div v-else>
+    <h1>No city found</h1>
   </div>
 </template>
 
