aeea956f83e9fbc5f6388809ce1d3a1be792df4c
Alex Dodd <alexdodd@gmail.com>
2021-01-16 01:03:11 +0000

    setup package env prettier eslint

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <hello-world></hello-world>
+    <test-component></test-component>
+    <div>
+      <p></p>
+    </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TestComponent from './components/TestComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    'hello-world': HelloWorld,
+    'test-component': TestComponent,
+  },
 }
 </script>
 
---
+++ src/components/NewComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Testing</div>
+</template>
+
+<script>
+export default {
+  name: "NewComponent",
+  components: []
+};
+</script>
+
+<style lang="sass" scoped></style>
