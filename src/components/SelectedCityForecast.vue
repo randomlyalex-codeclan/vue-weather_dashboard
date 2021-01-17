c0724821dc811e0640c5019da844c6e8a7e7dee0
Alex Dodd <alexdodd@gmail.com>
2021-01-17 23:48:06 +0000

    creation of the detailed forcast section lots more data wrangling needs to happen before anything needs to look nice

---
+++ src/components/SelectedCityForecast.vue
@@ -0,0 +1,73 @@
+<template>
+    <div>
+        <table>
+            <tr>
+                <th>Date/ Time</th>
+                <th>Summary</th>
+                <th>Cloud</th>
+                <th>Wind -></th>
+                <th>Wind Speed</th>
+                <th>Min/Max</th>
+                <th>Humidity</th>
+            </tr>
+            <tr></tr>
+            <tr v-for="(hourly, index) in forecast.list" v-bind:key="index">
+                <td>
+                    {{ convertDateTimeISO(hourly.dt, 8, 10) }}
+                    {{ convertDateTimeISO(hourly.dt, 11, 16) }}
+                </td>
+                <td>{{ hourly.weather[0].description }}</td>
+                <td>{{ hourly.clouds.all }}</td>
+                <!-- <span> {{ hourly.rain['3h'] }} </span> -->
+                <td>{{ hourly.wind.deg }}</td>
+                <td>{{ hourly.wind.speed }}</td>
+                <td>
+                    {{ roundToDp(hourly.main.temp_min - 273.15, 1) }} /
+                    {{ roundToDp(hourly.main.temp_max - 273.15, 1) }}
+                </td>
+                <td>{{ hourly.main.humidity }}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SelectedCityForecast',
+    data() {
+        return {
+            parsedDailyData: null,
+        }
+    },
+    props: ['forecast'],
+    methods: {
+        convertDateTime: function(timestamp, start, end) {
+            return new Date(timestamp * 1000).toString().substring(start, end)
+        },
+        convertDateTimeISO: function(timestamp, start, end) {
+            return new Date(timestamp * 1000)
+                .toISOString()
+                .substring(start, end)
+        },
+        roundToDp: function(num, dp) {
+            return Math.round((num + Number.EPSILON) * 10 ** dp) / 10 ** dp
+        },
+        // parseDailyData: function() {
+        //     parsedDailyData = {
+        //         mon: null,
+        //         tue: null,
+        //         wed: null,
+        //         thu: null,
+        //         fri: null,
+        //         sat: null,
+        //         sun: null,
+        //     }
+        // map with this: convertDateTimeISO(forcast.list[x].dt, 0, 3) === mon,tue,wed,thu,fri,sat,sun
+        //     this.forecast.list.map() // figure out how to map mon, tue,
+        // },
+    },
+    mounted: function() {},
+}
+</script>
+
+<style lang="css" scoped></style>
