647c7b99df6baa6b1e06f99cc86bf30e424d7946
Alex Dodd <alexdodd@gmail.com>
2021-01-18 00:56:03 +0000

    fixed my leaflet errors

--- src/main.js
+++ src/main.js
@@ -5,6 +5,15 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import { Icon } from 'leaflet'
+
+delete Icon.Default.prototype._getIconUrl
+Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+})
+
 new Vue({
     render: h => h(App),
 }).$mount('#app')
